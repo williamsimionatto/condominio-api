07c0631417f8f44ac4f7dfed89401e0aa026ef52
William <178344@upf.br>
2022-01-19 15:26:01 -0300

    refactor: confirmação de senha

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 class UserController extends Controller {
     public function getAll(Request $request) {
@@ -20,12 +21,15 @@ class UserController extends Controller {
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6|confirmed',
+            'password' => 'required|string|min:6',
         ]);
 
         if ($validator->fails()) {
             return response(['errors'=>$validator->errors()->all()], 422);
         }
+
+        $password = Hash::make($request->password);
+        $request->offsetSet('password', $password);
 
         $user = User::create($request->all());
         return response()->json($user);
@@ -35,7 +39,7 @@ class UserController extends Controller {
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6|confirmed',
+            'password' => 'required|string|min:6',
         ]);
 
         if ($validator->fails()) {
