99603878c3381117a602305956441bd808f2ed90
William <178344@upf.br>
2022-01-21 15:29:10 -0300

    feat: create PerfilRepository

--- app/Models/Perfil.php
+++ app/Models/Perfil.php
@@ -5,7 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Perfil extends Model
-{
+class Perfil extends Model {
     use HasFactory;
+
+    protected $fillable = ['name'];
 }
