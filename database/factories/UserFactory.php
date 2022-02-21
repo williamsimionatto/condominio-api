2dd6dc6d7bdb95bac8c35fe4b7fd7f1db6280f02
William <178344@upf.br>
2022-02-21 09:21:00 -0300

    create factories

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,11 +15,11 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
+            'name' => "Administrador",
+            'email' => "admin@mail.com",
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'active' => 'S',
+            'perfil_id' => '4'
         ];
     }
 
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
+    public function run() {
+        \App\Models\Perfil::factory(1)->create();
+        \App\Models\User::factory(1)->create();
     }
 }
