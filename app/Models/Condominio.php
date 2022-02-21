73ef565c429d0b47b9f7e260a8e141ed9903af4d
William <178344@upf.br>
2022-02-21 10:04:44 -0300

    create Condominio migration and model

---
+++ app/Models/Condominio.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Condominio extends Model {
+    use HasFactory;
+    protected $table = "condominio";
+
+    protected $fillable = [
+        'name',
+        'cnpj',
+        'condominio2quartos',
+        'condominio3quartos',
+        'condominiosalacomercial',
+        'valoragua',
+        'valorsalaofestas',
+        'valorlimpezasalaofestas',
+        'valormudanca'
+    ];
+}
---
+++ database/migrations/2022_02_21_124318_create_condominios_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCondominiosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('condominio', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('cnpj');
+            $table->double('condominio2quartos');
+            $table->double('condominio3quartos');
+            $table->double('condominiosalacomercial');
+            $table->double('valoragua');
+            $table->double('valorsalaofestas');
+            $table->double('valorlimpezasalaofestas');
+            $table->double('valormudanca');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('condominio');
+    }
+}
