1af3d1a502c70110f8f550a72783bedfebcd5df3
William <178344@upf.br>
2022-03-05 19:39:10 -0300

    bug fix

--- app/Repository/Interfaces/LeituraAguaRepositoryInterface.php
+++ app/Repository/Interfaces/LeituraAguaRepositoryInterface.php
@@ -5,5 +5,5 @@ namespace App\Repository\Interfaces;
 use App\Repository\EloquentRepositoryInterface;
 
 interface LeituraAguaRepositoryInterface extends EloquentRepositoryInterface {
-    public function getByCondominio($id): JsonResponse;
+    public function getByCondominio($id);
 }
