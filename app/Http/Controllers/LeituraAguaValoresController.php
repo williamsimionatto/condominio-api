2210a4e10f73f54526395c749f3e392aa8eee198
William <178344@upf.br>
2022-03-06 14:59:36 -0300

    ajuste query

--- app/Http/Controllers/LeituraAguaValoresController.php
+++ app/Http/Controllers/LeituraAguaValoresController.php
@@ -13,32 +13,28 @@ class LeituraAguaValoresController extends Controller {
         $data = $request->all();
 
         $results = DB::select(
-            "SELECT la.id, lav.leitura_agua AS leituraagua,
-                    CONCAT(c.condominio, '- ', c.name) AS condomino,
+            "SELECT '' AS id, '' AS leituraagua,
+                    CONCAT(c.apartamento , '- ', c.name) AS condomino,
                     CASE WHEN c.sindico = 'S' THEN 0.00
                         WHEN c.tipo = 'A' AND c.numeroquartos = 3 THEN co.condominio3quartos
                         WHEN c.tipo = 'A' AND c.numeroquartos = 2 THEN co.condominio2quartos 
                         WHEN c.tipo = 'S' THEN co.condominiosalacomercial 
-                    END valorcondominio, sub.consumo AS consumoAnterior, sub.consumo AS consumoAtual, 0 consumo,
-                    co.valoragua, lav.valorsalaofestas, lav.valorlimpezasalaofestas, lav.valormudanca, co.taxaboleto, 
+                    END valorcondominio, COALESCE(sub.consumo, 0) AS consumoAnterior, COALESCE(sub.consumo, 0) AS consumoAtual, 0 consumo,
+                    co.valoragua, co.valorsalaofestas, co.valorlimpezasalaofestas, co.valormudanca, co.taxaboleto, 
                     co.taxabasicaagua, 0 total
-            FROM leitura_agua la
-            JOIN leitura_agua_valores lav ON la.id = lav.leitura_agua
-            JOIN condomino c ON c.id = lav.id
-            JOIN condominio co ON co.id = la.condominio
-            JOIN (
+            FROM condomino c
+            LEFT JOIN condominio co ON co.id = c.condominio
+            LEFT JOIN (
                 SELECT lav.*
                 FROM leitura_agua_valores lav
                 JOIN leitura_agua la ON lav.leitura_agua  = la.id
                 WHERE EXTRACT(YEAR_MONTH FROM la.dataleitura) = EXTRACT(YEAR_MONTH FROM DATE_SUB(:dataLeitura, INTERVAL 1 MONTH))
-            ) AS sub ON sub.condomino = c.id 
-            WHERE la.id = :idLeitura",
+            ) AS sub ON sub.condomino = c.id
+            WHERE c.ativo = 'S'",
             [
-                'idLeitura' => $data['idLeitura'],
                 'dataLeitura' => $data['dataLeitura']
             ],
             [
-                'idLeitura' => 'integer',
                 'dataLeitura' => 'date'
             ]
         );
