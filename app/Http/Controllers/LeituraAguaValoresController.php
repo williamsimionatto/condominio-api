d41f51afd1d8b70ac65af0607f8f6a47aee24642
William <178344@upf.br>
2022-03-14 19:48:05 -0300

    refactor: change save method

--- app/Http/Controllers/LeituraAguaValoresController.php
+++ app/Http/Controllers/LeituraAguaValoresController.php
@@ -15,16 +15,11 @@ class LeituraAguaValoresController extends Controller {
         $data = $request->all();
         $data['leitura_agua'] = $data['leituraagua'];
         $condominio = Condominio::find($data['condominio']);
-        $data['condominio3quartos'] = $condominio->condominio3quartos;
-        $data['condominio2quartos'] = $condominio->condominio2quartos;
-        $data['condominiosalacomercial'] = $condominio->condominiosalacomercial;
-        // $data['valorsalaofestas'] = $condominio->valorsalaofestas;
-        // $data['valorlimpezasalaofestas'] = $condominio->valorlimpezasalaofestas;
-        // $data['valormudanca'] = $condominio->valormudanca;
-        $data['taxaboleto'] = $condominio->taxaboleto;
-        $data['taxabasicaagua'] = $condominio->taxabasicaagua;
-        $data['valoragua'] = $condominio->valoragua;
         $data['condomino'] = $data['condominoId'];
+        $data['consumo'] = $data['consumo'];
+        $data['qtdusosalao'] = $data['qtdusosalao'];
+        $data['qtdlimpezasalao'] = $data['qtdlimpezasalao'];
+        $data['qtdmudanca'] = $data['qtdmudanca'];
 
         $result = LeituraAguaValores::create($data);
         return response()->json($result);
@@ -32,69 +27,71 @@ class LeituraAguaValoresController extends Controller {
 
     public function getCondominos(Request $request) {
         $data = $request->all();
+        $results = [];
 
-        $results = DB::select(
-            "SELECT '' AS id, '' AS leituraagua,
-                    CONCAT(c.apartamento , '- ', c.name) AS condomino,
-                    CASE WHEN c.sindico = 'S' THEN 0.00
-                        WHEN c.tipo = 'A' AND c.numeroquartos = 3 THEN co.condominio3quartos
-                        WHEN c.tipo = 'A' AND c.numeroquartos = 2 THEN co.condominio2quartos 
-                        WHEN c.tipo = 'S' THEN co.condominiosalacomercial 
-                    END valorcondominio, COALESCE(sub.consumo, 0) AS consumoAnterior, COALESCE(sub.consumo, 0) AS consumoAtual, 0 consumo,
-                    co.valoragua, 0 valorsalaofestas, 0 valorlimpezasalaofestas, 0 valormudanca, co.taxaboleto, 
-                    co.taxabasicaagua, 0 total, c.id  AS condominoId
-            FROM condomino c
-            LEFT JOIN condominio co ON co.id = c.condominio
-            LEFT JOIN (
-                SELECT lav.*
-                FROM leitura_agua_valores lav
-                JOIN leitura_agua la ON lav.leitura_agua  = la.id
-                WHERE EXTRACT(YEAR_MONTH FROM la.dataleitura) = EXTRACT(YEAR_MONTH FROM DATE_SUB(:dataLeitura, INTERVAL 1 MONTH))
-            ) AS sub ON sub.condomino = c.id
-            WHERE c.ativo = 'S'",
-            [
-                'dataLeitura' => $data['dataLeitura']
-            ],
-            [
-                'dataLeitura' => 'date'
-            ]
-        );
+        // $results = DB::select(
+        //     "SELECT '' AS id, '' AS leituraagua,
+        //             CONCAT(c.apartamento , '- ', c.name) AS condomino,
+        //             CASE WHEN c.sindico = 'S' THEN 0.00
+        //                 WHEN c.tipo = 'A' AND c.numeroquartos = 3 THEN co.condominio3quartos
+        //                 WHEN c.tipo = 'A' AND c.numeroquartos = 2 THEN co.condominio2quartos 
+        //                 WHEN c.tipo = 'S' THEN co.condominiosalacomercial 
+        //             END valorcondominio, COALESCE(sub.consumo, 0) AS consumoAnterior, COALESCE(sub.consumo, 0) AS consumoAtual, 0 consumo,
+        //             co.valoragua, 0 valorsalaofestas, 0 valorlimpezasalaofestas, 0 valormudanca, co.taxaboleto, 
+        //             co.taxabasicaagua, 0 total, c.id  AS condominoId
+        //     FROM condomino c
+        //     LEFT JOIN condominio co ON co.id = c.condominio
+        //     LEFT JOIN (
+        //         SELECT lav.*
+        //         FROM leitura_agua_valores lav
+        //         JOIN leitura_agua la ON lav.leitura_agua  = la.id
+        //         WHERE EXTRACT(YEAR_MONTH FROM la.dataleitura) = EXTRACT(YEAR_MONTH FROM DATE_SUB(:dataLeitura, INTERVAL 1 MONTH))
+        //     ) AS sub ON sub.condomino = c.id
+        //     WHERE c.ativo = 'S'",
+        //     [
+        //         'dataLeitura' => $data['dataLeitura']
+        //     ],
+        //     [
+        //         'dataLeitura' => 'date'
+        //     ]
+        // );
 
         return response()->json($results);
     }
 
     public function getValoresCondominos(Request $request) {
         $data = $request->all();
-        $results = DB::select(
-                "SELECT la.id, lav.leitura_agua AS leituraagua,
-                    CONCAT(c.apartamento , '- ', c.name) AS condomino,
-                    CASE WHEN c.sindico = 'S' THEN 0.00
-                        WHEN c.tipo = 'A' AND c.numeroquartos = 3 THEN co.condominio3quartos
-                        WHEN c.tipo = 'A' AND c.numeroquartos = 2 THEN co.condominio2quartos 
-                        WHEN c.tipo = 'S' THEN co.condominiosalacomercial 
-                        END valorcondominio, COALESCE(sub.consumo, 0) AS consumoAnterior, lav.consumo AS consumoAtual, ABS(lav.consumo - COALESCE(sub.consumo, 0)) AS consumo,
-                        co.valoragua, lav.valorsalaofestas, lav.valorlimpezasalaofestas, lav.valormudanca, co.taxaboleto, 
-                        co.taxabasicaagua, 0 total, c.id  AS condominoId
-                FROM leitura_agua la
-                JOIN leitura_agua_valores lav ON la.id = lav.leitura_agua
-                JOIN condomino c ON c.id = lav.condomino 
-                JOIN condominio co ON co.id = la.condominio
-                LEFT JOIN (
-                    SELECT lav.*
-                    FROM leitura_agua_valores lav
-                    JOIN leitura_agua la ON lav.leitura_agua  = la.id
-                    WHERE EXTRACT(YEAR_MONTH FROM la.dataleitura) = EXTRACT(YEAR_MONTH FROM DATE_SUB(:dataLeitura, INTERVAL 1 MONTH))
-                ) AS sub ON sub.condomino = c.id
-                WHERE lav.leitura_agua = :idLeitura",
-            [
-                'idLeitura' => $data['idLeitura'],
-                'dataLeitura' => $data['dataLeitura']
-            ],
-            [
-                'idLeitura' => 'int',
-                'dataLeitura' => 'date'
-            ]
-        );
+        $result = [];
+        // $results = DB::select(
+        //         "SELECT la.id, lav.leitura_agua AS leituraagua,
+        //             CONCAT(c.apartamento , '- ', c.name) AS condomino,
+        //             CASE WHEN c.sindico = 'S' THEN 0.00
+        //                 WHEN c.tipo = 'A' AND c.numeroquartos = 3 THEN co.condominio3quartos
+        //                 WHEN c.tipo = 'A' AND c.numeroquartos = 2 THEN co.condominio2quartos 
+        //                 WHEN c.tipo = 'S' THEN co.condominiosalacomercial 
+        //                 END valorcondominio, COALESCE(sub.consumo, 0) AS consumoAnterior, lav.consumo AS consumoAtual, ABS(lav.consumo - COALESCE(sub.consumo, 0)) AS consumo,
+        //                 co.valoragua, lav.valorsalaofestas, lav.valorlimpezasalaofestas, lav.valormudanca, co.taxaboleto, 
+        //                 co.taxabasicaagua, 0 total, c.id  AS condominoId
+        //         FROM leitura_agua la
+        //         JOIN leitura_agua_valores lav ON la.id = lav.leitura_agua
+        //         JOIN condomino c ON c.id = lav.condomino 
+        //         JOIN condominio co ON co.id = la.condominio
+        //         LEFT JOIN (
+        //             SELECT lav.*
+        //             FROM leitura_agua_valores lav
+        //             JOIN leitura_agua la ON lav.leitura_agua  = la.id
+        //             WHERE EXTRACT(YEAR_MONTH FROM la.dataleitura) = EXTRACT(YEAR_MONTH FROM DATE_SUB(:dataLeitura, INTERVAL 1 MONTH))
+        //         ) AS sub ON sub.condomino = c.id
+        //         WHERE lav.leitura_agua = :idLeitura",
+        //     [
+        //         'idLeitura' => $data['idLeitura'],
+        //         'dataLeitura' => $data['dataLeitura']
+        //     ],
+        //     [
+        //         'idLeitura' => 'int',
+        //         'dataLeitura' => 'date'
+        //     ]
+        // );
 
         return response()->json($results);
     }
