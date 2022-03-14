08858303033508da2fafa59862a57b0f69ccff57
William <178344@upf.br>
2022-03-14 18:20:21 -0300

    feat: create Repository for HistoricoValoresCondominio

---
+++ app/Repository/Interfaces/HistoricoValoresCondominioInterfaces.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Repository\Interfaces;
+
+use App\Repository\EloquentRepositoryInterface;
+
+interface HistoricoValoresCondominioRepositoryInterface extends EloquentRepositoryInterface {}
