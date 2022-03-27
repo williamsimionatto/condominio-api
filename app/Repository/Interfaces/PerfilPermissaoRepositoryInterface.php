e50b0524fb6705da35b1bc5a6858d7cba4066447
William <178344@upf.br>
2022-03-27 19:03:56 -0300

    feat: verify permissions into condominio routes

--- app/Repository/Interfaces/PerfilPermissaoRepositoryInterface.php
+++ app/Repository/Interfaces/PerfilPermissaoRepositoryInterface.php
@@ -6,5 +6,6 @@ use App\Repository\EloquentRepositoryInterface;
 
 interface PerfilPermissaoRepositoryInterface extends EloquentRepositoryInterface {
     public function getPermissoesByPerfil(Int $idPerfil);
+    public function getPermissoesByPerfilAndSigla(Int $idPerfil, String $sigla);
     public function deletePermissoesByPerfil($idPerfil);
 }
