d867f62d7d69c0072a5b324131d93dae058a8915
William <178344@upf.br>
2022-04-03 18:40:42 -0300

    refactor: using response download

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -41,15 +41,11 @@ class FileUploadController extends Controller {
         file_put_contents($fileName, $file);
 
         if (file_exists($fileName)) {
-            header('Content-Description: File Transfer');
-            header('Content-Type: application/octet-stream');
-            header('Content-Disposition: attachment; filename="'.basename($fileName).'"');
-            header('Expires: 0');
-            header('Cache-Control: must-revalidate');
-            header('Pragma: public');
-            header('Content-Length: ' . filesize($fileName));
-            readfile($fileName);
-            exit;
+            return response()->download($fileName, 'boleto.pdf', [
+                'Content-Length: ' . filesize($fileName),
+            ], 'attachment');
         }
+
+        return response()->json(['success' => false, 'message' => 'Erro ao baixar arquivo.'], 500);
     }
 }
