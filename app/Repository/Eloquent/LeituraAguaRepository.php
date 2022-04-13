afdfea0a1d2cc0b2aa4719fe1b45887ad947ca52
William <178344@upf.br>
2022-04-13 19:38:25 -0300

    feat: oder by query

--- app/Repository/Eloquent/LeituraAguaRepository.php
+++ app/Repository/Eloquent/LeituraAguaRepository.php
@@ -19,6 +19,7 @@ class LeituraAguaRepository extends BaseRepository implements LeituraAguaReposit
     public function getAll() {
         return $this->model->join('condominio', 'condominio.id', '=', 'leitura_agua.condominio')
             ->select('leitura_agua.*', 'condominio.name as condominio', 'condominio.id as condominioid')
+            ->orderBy('leitura_agua.dataleitura', 'desc')
             ->get();
     }
 
