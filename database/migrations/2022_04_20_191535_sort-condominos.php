23923d4c60cb32ac9dd59fd189c3a7d3ff5212eb
William <178344@upf.br>
2022-04-20 16:18:36 -0300

    feat: create migration

---
+++ database/migrations/2022_04_20_191535_sort-condominos.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class SortCondominos extends Migration {
+    public function up() {
+        Schema::table('condomino', function (Blueprint $table) {
+            $table->integer('position')->nullable()->default(0);
+        });
+    }
+
+    public function down()    {
+        Schema::table('condomino', function (Blueprint $table) {
+            $table->dropColumn('position');
+        });
+    }
+}
