6dd65487ca9d9b717dc2f8d8d8dd200eacec23a6
William <178344@upf.br>
2022-05-07 11:43:33 -0300

    fix: ajustes cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -7,8 +7,10 @@ use Illuminate\Http\Request;
 
 class Cors {
     public function handle(Request $request, Closure $next) {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response = $next($request);
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization');
+        return $response;
     }
 }
